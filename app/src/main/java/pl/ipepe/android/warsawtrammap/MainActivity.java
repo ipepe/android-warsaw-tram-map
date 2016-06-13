cbcaebade16bda55ef0378cb4ce1956bd3368e11
Patryk Ptasiński <patryk.ptasinski@ipepe.pl>
2016-06-14 01:12:06 +0200

    Added polish strings

--- app/src/main/java/pl/ipepe/android/warsawtrammap/MainActivity.java
+++ app/src/main/java/pl/ipepe/android/warsawtrammap/MainActivity.java
@@ -2,12 +2,56 @@ package pl.ipepe.android.warsawtrammap;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
+
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+//        webView.setWebViewClient(new WebViewClient());
+//        webView.setWebChromeClient(new WebChromeClient() {
+//            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+//                callback.invoke(origin, true, false);
+//            }
+//        });
+        WebSettings webSettings = webView.getSettings();
+//        webSettings.setDomStorageEnabled(true);
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/www/index.html");
+
     }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu)
+//    {
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item)
+//    {
+//        switch (item.getItemId()) {
+//            case R.id.one:
+//                Toast.makeText(this, "Jeden klik", Toast.LENGTH_SHORT).show();
+//                return true;
+//            case R.id.two:
+//                Toast.makeText(this, "Dwa kliki", Toast.LENGTH_SHORT).show();
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
 }
